e6d835591071f316d273fa7438adcfc153dcccdf
Monikku12 <monique.paymentboulanger@gmail.com>
2022-06-11 04:07:11 +0900

    game done. Working on stats.

--- monty_hall_problem.js
+++ monty_hall_problem.js
@@ -7,16 +7,20 @@ class Door {
 }
 
 class Game {
-    constructor() {
+    constructor(choice) {
         this.doors = [];
         this.doorPicked;
         this.openedGoatDoor;
         this.finalPick;
         this.won;
+        this.goatDoor;
+        this.choice = choice;
         this.createDoors();
         this.firstTurn();
         this.secondTurn();
         this.thirdTurn();
+        this.results();
+        
     }
     
     createDoors() {
@@ -28,64 +32,51 @@ class Game {
 
     firstTurn() {
         this.doorPicked = this.doors[Math.floor(Math.random() * (3 - 1 + 1) )]
-        console.log("firstTurn is ", this.doorPicked)
+        console.log("You chose", this.doorPicked)
     }
 
     secondTurn() {        
-        this.openedGoatDoor = this.doors.filter(door=>
+        this.goatDoor = this.doors.filter(door=>
             door.number !== this.doorPicked.number && door.isCar == false); 
-            this.openedGoatDoor[0].opened = true;
-        console.log("secondTurn is :", this.openedGoatDoor)
+            this.goatDoor[0].opened = true;
+        this.openedGoatDoor = this.goatDoor.filter(door=>
+            door.opened == true);
+        console.log("Monty opened", this.openedGoatDoor)
     }
 
-// TODO: - Check Why "this.finalPick[0].opened = true" don't return 
     thirdTurn() {
         this.finalPick = this.doors.filter(door=>
-            door.number !== this.doorPicked.number && this.openedGoatDoor.number);
-            this.finalPick[0].opened = true;
-        console.log("thirdTurn is :", this.doorPicked.number)
+            door.opened == false && door.number !== this.doorPicked.number);
+        this.finalPick[0].opened = true;
+        console.log("You changed door and opened", this.finalPick)
     }
     
-    Results() {  
-        var choice;
-        var sameDoor;
-        var doorChange;
-        if (choice = sameDoor) {
-            if (this.doorPicked.isCar = true) {
+    results() {
+        if (this.choice == true) {
+            if (this.doorPicked.isCar == true) {
                 this.doorPicked.opened = true;
-                console.log("this.doorPicked is", this.doorPicked)
-                alert("You win!");
                 this.won = true;
             } else {
                 this.doorPicked.opened = true;
-                alert("You loose!");
                 this.won = false;
             }
-        } else (choice = doorChange)
-        if (this.finalPick.isCar = true) {
-            this.finalPick.opened = true;
-            console.log("this.finalPick is", this.finalPick)
-            alert("You win!");
-            this.won = true;
-        } else {
-            this.finalPick.opened = true;
-            alert("You loose!");
-            this.won = false;
-        }
+        } else if (this.choice == false) {
+            if (this.finalPick.isCar == true) {
+                this.finalPick.opened = true;
+                this.won = true;
+            } else {
+                this.finalPick.opened = true;
+                this.won = false;                
+            }
+        } console.log("You win is", this.won)
     }
 }
-
-var sameDoorGame = new Game(choice = sameDoor);
-var doorChangeGame = new Game(choice = doorChange);
-
-// for (let i = 0; i < 10000; i++) {
-//     // if statement game play
-//    }
 
 
 
 class Statistics {
     constructor() {
+        // this.gameNumber = gameNumber;
         this.gamesWithSameDoorWon = [];
         this.gamesWithSameDoorLost = [];
         this.gamesWithDoorChangeWon = [];
@@ -96,7 +87,6 @@ class Statistics {
         this.percentageGamesWithSameDoorLost;
         this.percentageGamesWithDoorChangeWon;
         this.percentageGamesWithDoorChangeLost;
-
     }
 
     function () {
@@ -104,27 +94,54 @@ class Statistics {
         return (percentageGamesWithSameDoorWon, "%")
     }
 
-    function () {
-        percentageGamesWithSameDoorLost = gamesWithSameDoorLost / totalSameDoorPlayedGames * 100
-        return (percentageGamesWithSameDoorLost, "%")
-    }
+//     function () {
+//         percentageGamesWithSameDoorLost = gamesWithSameDoorLost / totalSameDoorPlayedGames * 100
+//         return (percentageGamesWithSameDoorLost, "%")
+//     }
 
-    function () {
-        percentageGamesWithDoorChangeWon = gamesWithDoorChangeWon / totalDoorChangePlayedGames * 100
-        return (percentageGamesWithDoorChangeWon, "%")
-    }
+//     function () {
+//         percentageGamesWithDoorChangeWon = gamesWithDoorChangeWon / totalDoorChangePlayedGames * 100
+//         return (percentageGamesWithDoorChangeWon, "%")
+//     }
 
-    function () {
-        percentageGamesWithDoorChangeLost = gamesWithDoorChangeLost / totalDoorChangePlayedGames * 100
-        return (percentageGamesWithDoorChangeLost, "%")
-    }
+//     function () {
+//         percentageGamesWithDoorChangeLost = gamesWithDoorChangeLost / totalDoorChangePlayedGames * 100
+//         return (percentageGamesWithDoorChangeLost, "%")
+//     }
 }
-// let game = 0
 
-while (game <= 10000) {
-    // game play
-    game++;
-  }
-  totalPlayedGames = game(value)
 
-  console.log("End game")
+var data = new Statistics;
+
+let count = 0
+// Game(choice) : true = sameDoor, false = doorChange
+for (let i = 0; i < 10; i++) {
+    var sameDoorGame = new Game(true); 
+    count++;   
+    if (sameDoorGame.won = true) {
+        data.gamesWithSameDoorWon++;
+        console.log("Well played")
+    }
+    // sameDoorGame.won = false
+    // console.log(this.gamesWithSameDoorWon)
+    }
+
+    
+// for (let i = 0; i < 10; i++) {
+//     var doorChangeGame = new Game(false);
+//     }
+
+// for (let gameNumber = 0; gameNumber < 10; gameNumber++) {
+//     var doorChangeGame = new Game(false);
+    
+//     }
+
+
+// while (gameNumber = 0, gameNumber <= 10) {
+//     let 
+//     var sameDoorGame = new Game(true);
+//     this.gameNumber++;
+//     }
+//     totalSameDoorPlayedGames = gameNumber
+
+  console.log("Game Over")
